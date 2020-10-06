3aca874f805cd5f036d3c3abb43615d055d0edb4
Cyril Carral <cyril.carral85@gmail.com>
2020-10-06 21:42:31 +0200

    add router + center elements

--- src/main.js
+++ src/main.js
@@ -3,16 +3,18 @@ import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import store from './store/'
-import VueRouter from 'vue-router'
+import router from './router'
 
-Vue.use(VueRouter)
+Vue.use(router)
 
 
 new Vue({
   // Injection du store à la racine pour l'utiliser sur tous les composants
   store,
+  // Mise à dispo des routes pour toute l'app
+  router,
   el: '#app',
-  render: h => h(App),
+  render: h => h(App)
 })
 
 
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <container></container>
+    <header-top></header-top>
+  </div>
+</template>
+
+<script>
+import Container from '../components/Container/Container'
+import Header from '../components/Header/Header'
+export default {
+  name: 'Home',
+  components: {
+   'container': Container,
+   'header-top': Header
+  }
+}
+</script>
+
+<style>
+  @import '../App.css';
+</style>
